7ac18aa6c18cf77befc6c031e88cfba884a77f0c
kgcloud <cnjuguna@student.s19.be>
2021-09-27 18:51:45 +0200

    ajout des 2 fractols

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: canjugun <canjugun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 16:21:50 by canjugun          #+#    #+#             */
-/*   Updated: 2021/09/21 17:44:36 by canjugun         ###   ########.fr       */
+/*   Updated: 2021/09/27 18:50:56 by canjugun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,6 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-typedef struct	s_fractol{
-	double cre;
-	double cim;
-	double newre;
-	double newim;
-	double oldre;
-	double oldim;
-	double zoom;
-	double movex;
-	double movey;
-	int		maxi;
-}				t_fractol;
-
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -42,63 +29,140 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int	main(void)
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+void	mandelbrot(void)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	t_fractol kg;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 400, 300, "Fractol!");
-	img.img = mlx_new_image(mlx, 400, 300);
+	mlx_win = mlx_new_window(mlx, 1200, 900, "mandelbrot!");
+	img.img = mlx_new_image(mlx, 1200, 900);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	kg.zoom = 1;
-	kg.movex = 0;
-	kg.movey = 0;
-	kg.cre = -0.7;
-	kg.cim = 0.27015;
-	int y = 0;
-	int x = 0;
-	int h = 400;
-	int w = 300;
-	while (y < h)
-	{
-		while(x < w)
-		{
-			kg.newre = 1.5 * (x - w / 2) / (0.5 * kg.zoom * w) + kg.movex;
-			kg.newim = (y - h / 2) / (0,5 * kg.zoom * h) + kg.movey;
+	
+	int couleurs;
+	int r = 80;
+	int g = 150;
+	int b = 200;	
+	double pr, pi;
+	double newre, newim, oldre, oldim;
+	double zoom = 1, movex = -0.5, movey = 0;
+	int maxi = 900;
+	int h = 900;
+	int w = 1200;
+	
+	for(int y = 0; y < h; y++)
+	{	
+		for (int x = 0 ; x < w; x++)
+		{	
+			pr = 1.5 * (x - w /2) / (0.5 * zoom * w) + movex;
+			pi = (y - h / 2) / (0.5 * zoom * h) + movey;
+			newre = newim = oldre = oldim = 0;
 			int i;
-			i = -1;
-			while (i < kg.maxi)
+			for(i = 0; i < maxi; i++)
 			{
-				i++;
-				kg.oldre = kg.newre;
-				kg.oldim = kg. newim;
-				
-				kg.newre = kg.oldre * kg.oldre - kg.oldim * kg.oldim + kg.cre;
-				kg.newim = 2 * kg.oldre * kg.oldim + kg.cim;
-				if((kg.newre * kg.newre + kg.newim * kg.newim) > 4) 
+				oldre = newre;
+				oldim = newim;
+				newre = oldre * oldre - oldim * oldim + pr;
+				newim = 2 * oldre * oldim + pi;
+				if ((newre * newre + newim * newim) > 4)
 					break;
 			}
-			my_mlx_pixel_put(&img, x, y, 0x00FF0000);
-			x++;
+			if (r > 255)
+				r = 0;
+			if (g > 255)
+				g = 0;
+			if (b > 255)
+				b = 0;
+			couleurs = create_trgb(0, r, g, b);
+			if (i != maxi)
+				my_mlx_pixel_put(&img, x, y, couleurs);
+			else
+				my_mlx_pixel_put(&img, x, y, 00000000);
+			r++;
+			g++;
+			b++;
 		}
-		y++;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+}
+
+void	julia(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1200, 900, "julia!");
+	img.img = mlx_new_image(mlx, 1200, 900);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	
+	int couleurs;
+	int r = 80;
+	int g = 150;
+	int b = 200;
+	double cre, cim;
+	double newre, newim, oldim, oldre;
+	double zoom = 1, movex = 0, movey = 0;
+	int maxi = 250;
+	int h = 900;
+	int w = 1200;
+
+	cre = -0.7;
+	cim = 0.27015;
+	
+	for(int y = 0; y < h; y++)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			newre = 1.5 * (x - w / 2) / (0.5 * zoom * w) + movex;
+			newim = (y - h / 2) / (0.5 * zoom * h) + movey;
+			int i;
+			
+			for(i = 0; i < maxi; i++)
+			{
+				oldre = newre;
+				oldim = newim;
+				
+				newre = oldre * oldre - oldim * oldim +cre;
+				newim = 2 * oldre * oldim + cim;
+				if ((newim * newim + newre * newre) > 4)
+					break;
+			}
+			if (r > 255)
+				r = 0;
+			if (g > 255)
+				g = 0;
+			if (b > 255)
+				b = 0;
+			if (i != maxi)
+			{	
+				couleurs = create_trgb(0, i % 256, g, 255 * (i < maxi));
+				my_mlx_pixel_put(&img, x, y, couleurs);
+			}
+			else
+				my_mlx_pixel_put(&img, x, y, 00000000);
+			r++;
+			g++;
+			b++;
+		}
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
+
+int		main(int ac, char **av)
+{
+	if (av[1][0] == 'm')
+		mandelbrot();
+	julia();
 }
--- start.c
+++ start.c
@@ -1,9 +1,60 @@
-int		main()
-{
-	int i_max = 50;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   start.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: canjugun <canjugun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/26 22:39:05 by canjugun          #+#    #+#             */
+/*   Updated: 2021/09/26 22:49:38 by canjugun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "fractol.h"
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
 
-https://www.geeksforgeeks.org/fractals-in-cc/
-https://lodev.org/cgtutor/juliamandelbrot.html
-https://www.carnetdumaker.net/articles/dessiner-la-fractale-de-mandelbrot-en-python-et-en-cc/
+int		main()
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 400, 300, "Fractol!");
+	img.img = mlx_new_image(mlx, 400, 300);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	double	x1 = 2.1;
+	double	x2 = 0.6;
+	double	y1 = -1.2;
+	double	y2 = 1.2;
+	int		zoom = 100;
+	int		imax = 50;
+
+	double image_x = (x2 - x1) * zoom;
+	double image_y = (y2 - y1) * zoom;
+	
+	int i = 0;
+	int couleur[imax + 1];
+	while (i < imax)
+	{
+		couleur[i] = 
+		i++;
+	}
+}
